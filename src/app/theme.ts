95620eb6c95817e248a9af1a2a9dd20664d03c4b
JIMIN PARK <103014298+keepinblazing@users.noreply.github.com>
2023-09-22 21:56:45 +0900

    feat: theme 설정 추가

---
+++ src/app/theme.ts
@@ -0,0 +1,13 @@
+import { Theme } from "@emotion/react";
+
+const theme: Theme = {
+  colors: {
+    black: "#000",
+    white: "#fff",
+  },
+  fonts: {
+    inter: "Inter",
+  },
+};
+
+export default theme;
