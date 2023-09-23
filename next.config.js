49ce178be1caba8aa73afb89229608ddfaf53f4a
JIMIN PARK <103014298+keepinblazing@users.noreply.github.com>
2023-09-23 13:11:43 +0900

    feat: add 폴더 구조

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  redirects: async () => {
+    return [
+      {
+        source: '/home',
+        destination: '/lecture',
+        permanent: true,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
