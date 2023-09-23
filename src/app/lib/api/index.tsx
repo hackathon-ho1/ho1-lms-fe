3bfd16c6b45d265d3c3aa9a790b321525208c3b6
JIMIN PARK <103014298+keepinblazing@users.noreply.github.com>
2023-09-23 19:10:44 +0900

    feat: add fetch 기본 세팅

--- src/app/lib/api/index.tsx
+++ src/app/lib/api/index.tsx
@@ -1,1 +1,29 @@
-export {};
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+
+type RequestOptions = Partial<RequestInit> & {
+  endpoint: string;
+};
+
+const apiRequest = async (options: RequestOptions) => {
+  const { endpoint, ...rest } = options;
+  const defaultHeaders = {
+    'Content-Type': 'application/json',
+  };
+  const mergedOptions: RequestInit = {
+    ...rest,
+    headers: {
+      ...defaultHeaders,
+      ...rest.headers,
+    },
+  };
+  const response = await fetch(`${BASE_URL}${endpoint}`, mergedOptions);
+  if (!response.ok) {
+    throw new Error('API request failed');
+  }
+  const data = await response.json();
+  return data;
+};
+
+export const getGotgamList = async (month: number) => {
+  return apiRequest({ endpoint: `/api/gotgaom?=${month}` });
+};
