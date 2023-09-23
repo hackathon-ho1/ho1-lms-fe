18d7ca130b3235e16c4f422b2ba05252c89f8fa8
yeun38 <choh1997@naver.com>
2023-09-24 01:22:04 +0900

    feat : add 무한 스크롤

--- src/app/lecture/page.tsx
+++ src/app/lecture/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { LectureList } from '../../../lectureSample.json';
 
 export type Course = {
@@ -12,6 +12,37 @@ export type Course = {
 
 export default function LecturePage() {
   const [list, setList] = useState<Course[]>(LectureList);
+  const [last, setLast] = useState<Course>({} as Course);
+
+  const [lastIntersectingImage, setLastIntersectingImage] = useState<HTMLDivElement | null>(null);
+
+  const getListThenSet = async () => {};
+
+  const onIntersect: IntersectionObserverCallback = (entries, observer) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        //뷰포트에 마지막 이미지가 들어오고, page값에 1을 더하여 새 fetch 요청을 보내게됨 (useEffect의 dependency배열에 page가 있음)
+        // 현재 타겟을 unobserve한다.
+        observer.unobserve(entry.target);
+      }
+    });
+  };
+
+  useEffect(() => {
+    console.log('page ? ', last);
+    getListThenSet();
+  }, [last]);
+
+  useEffect(() => {
+    //observer 인스턴스를 생성한 후 구독
+    let observer: IntersectionObserver;
+    if (lastIntersectingImage) {
+      observer = new IntersectionObserver(onIntersect, { threshold: 0.5 });
+      //observer 생성 시 observe할 target 요소는 불러온 이미지의 마지막아이템(randomImageList 배열의 마지막 아이템)으로 지정
+      observer.observe(lastIntersectingImage);
+    }
+    return () => observer && observer.disconnect();
+  }, [lastIntersectingImage]);
 
   return (
     <div>
